55c589424bac0a6360dca35b9f30985b70177199
DUMBRAVA <C.Dumbrava@pentanova.com>
2024-05-28 09:25:07 +0300

    PageObjectModel

---
+++ src/main/java/selenium/utils/BaseTest.java
@@ -0,0 +1,30 @@
+package selenium.utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+	
+public WebDriver driver;
+	
+	@BeforeClass 
+	public void setup() throws InterruptedException {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // se adauga in Base Test si nu in @Test pentru ca e o actiune generala
+		driver.get("https://keybooks.ro");
+		
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000); //pauza de 5sec executiei, este bad practice in testarea automata
+		driver.quit();	
+	}
+
+}
