9530d2a40525ea356bd3aee01ccb1442b0f2b048
DUMBRAVA <C.Dumbrava@pentanova.com>
2024-06-03 21:32:48 +0300

    curs22 xpath

--- src/main/java/tests/XpathExample.java
+++ src/main/java/tests/XpathExample.java
@@ -25,7 +25,7 @@ public class XpathExample extends BaseTest{
 	 */
 
 	@Test(priority=1)
-	public void xpatheExample() {
+	public void xpathExample() throws InterruptedException {
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 	
 		WebElement loginButton = driver.findElement(By.xpath("//li[@class='menu_user_login']"));
@@ -34,12 +34,70 @@ public class XpathExample extends BaseTest{
 				+ "('style', 'background:yellow; border:4px solid green;')", loginButton);
 		loginButton.click();
 		
-		WebElement userField = driver.findElement(By.xpath("//ul//input[@id='log' or @name='log']")); //logical OR
+		 //logical OR
+		WebElement userField = driver.findElement(By.xpath("//ul//input[@id='log' or @name='log']"));
 		jse.executeScript("arguments[0].setAttribute"
 				+ "('style', 'background:yellow; border:4px solid green;')", userField);
 		userField.sendKeys(USER);
 	
+		//logical AND
+		WebElement passField = driver.findElement(By.xpath("//ul//input[@type='password' and @name='pwd']"));
+		jse.executeScript("arguments[0].setAttribute"
+				+ "('style', 'background:yellow; border:4px solid green;')", passField);
+		passField.sendKeys(PASS);
+		
+		// direct child (o singura treapta), la xpath este un singur slash /
+		WebElement submitButton = driver.findElement(By.xpath("//ul//div[@class='popup_form_field submit_field']/input"));
+		submitButton.click();
+		
+		Thread.sleep(4000);	
+		
 }
-	
+	@Test(priority=2)
+	public void xpathExample2() throws InterruptedException {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		
+		WebElement userName= driver.findElement(By.xpath("//span[@class='user_name']"));
+		jse.executeScript("arguments[0].setAttribute"
+				+ "('style', 'background:yellow; border:4px solid green;')", userName);
+		userName.click();
+		
+		//<a href="https://keybooks.ro/wp-admin/profile.php" class="icon icon-cog">Settings</a>
+		
+		//  text()  --> referinta catre textul dintre tag-urile HTML
+		WebElement textSettings = driver.findElement(By.xpath("//a[text()='Settings']"));
+		textSettings.click();
+		
+		
+		
+		/*
+		 * <a href="https://keybooks.ro/account/orders/">recent orders</a>
+		 * 
+		 *  //a[text()='recent orders']
+		 * 
+		 *  //a[contains(text(),'rece' )]  --> cand vrem sa cautam doar o parte din text, spre ex rece de la recent
+		 *  
+		 *  //a[contains(@href, '/account/orders/')] --> folosim contains pe atribut
+		 */
+		
+		WebElement recentOrders = driver.findElement(By.xpath("//a[contains(text(),'rece')]"));
+		jse.executeScript("arguments[0].setAttribute"
+				+ "('style', 'background:yellow; border:4px solid green;')", recentOrders);
+		recentOrders.click();
+		
+		WebElement ordersTabHeader = driver.findElement(By.xpath("//th[contains(@class, 'header-order-number')]/span"));
+		jse.executeScript("arguments[0].setAttribute"
+				+ "('style', 'background:yellow; border:4px solid green;')", ordersTabHeader);
+		
+		WebElement dateTabHeader = driver.findElement(By.xpath("//th[contains(@class, 'header-order-date')]/span[text()='Date']"));
+		jse.executeScript("arguments[0].setAttribute"
+				+ "('style', 'background:yellow; border:4px solid green;')", dateTabHeader);
+		
+		// select pe baza de index --> (//span[@class='nobr'])[3]
+		WebElement statusTabHeader = driver.findElement(By.xpath("(//span[@class='nobr'])[3]"));
+		jse.executeScript("arguments[0].setAttribute"
+				+ "('style', 'background:yellow; border:4px solid green;')", statusTabHeader);
+		
+	}
 	
 }
